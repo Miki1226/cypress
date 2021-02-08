4e60e25a8e87044bb8fdd93f0990893470d35d74
Mikolaj <mikkozlowski@gmail.com>
2021-02-08 21:03:40 +0100

    Registration and menu names validation test

--- cypress/integration/first_test.spec.js
+++ cypress/integration/first_test.spec.js
@@ -12,3 +12,42 @@ describe('First suite', () => {
 
     })
 })
+
+describe('automationPractice suite', () => {
+
+    describe('My suite section', () => {
+
+        it('registration - positive path', () => {
+
+            cy.visit('http://automationpractice.com/index.php')
+            cy.get('.container').find('.login').click()
+            cy.get('#create-account_form').should('contain', 'Create an account').then(form => {
+                cy.wrap(form).find('#email_create').click().type('mik@mik.pl')
+                cy.wrap(form).find('#SubmitCreate').click()
+            })
+
+
+        })
+        it.only('validate menu names', () => {
+
+            cy.visit('http://automationpractice.com/index.php')
+
+            cy.get('#block_top_menu').then((menu) => {
+                cy.wrap(menu).find('.sf-menu')
+                    .find('[title="Women"]')
+                    .should('contain', 'Women')
+
+
+                cy.wrap(menu).find('.sf-menu')
+                    .find('[title="Dresses"]')
+                    .should('contain', 'Dresses')
+
+
+                cy.wrap(menu).find('.sf-menu')
+                    .find('[title="T-shirts"]')
+                    .should('contain', 'T-shirts')
+
+            })
+        })
+    })
+})
